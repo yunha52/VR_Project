ae30012255003e2af6339ee1fb162a67e5f8ec83
yunha52 <okillis@hanmail.net>
2025-05-14 01:11:12 +0900

    Initial commit

---
+++ Assets/Gun.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // 총알의 갯수 - 데이터
+    // 1개,2개,3개
+    public int bulletCount = 10;
+    public Rigidbody bullet;
+    public Transform firePos;
+
+    // 총알을 발사 - 기능[함수]
+    // 총을 쏜다
+    public void Shoot()
+    {
+        // 오브젝트를 생성
+        //Instantiate(bullet);
+
+        // 오브젝트를 생성 - firePos 안쪽에 위치
+        //Instantiate(bullet, firePos);
+
+        // 오브젝트를 생성 - 벡터의 위치에 생성, Quaternion의 회전값
+        Rigidbody bulletObj = Instantiate(bullet, firePos.position, firePos.rotation);
+        bulletObj.AddForce(firePos.forward * 100, ForceMode.Impulse);
+    }
+}
